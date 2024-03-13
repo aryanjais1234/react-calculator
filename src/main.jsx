3a6b82ce4f7b1c3c8147850e274b6ad0a76036ea
aryanjais1234 <aryanjais1234@gmail.com>
2024-03-14 01:37:21 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css"
+import styles from "./Main.module.css";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <div className={styles.container}>
     <App />
+    </div>
   </React.StrictMode>,
 )
